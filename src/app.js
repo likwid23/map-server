98efb04405561d68b6f38dfb69c6c72f3ecf94af
likwid23 <likwid2@hotmail.com>
2020-11-10 00:25:33 -0500

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
-
+const db = require('../db');
+const messages = db.get('messages');
 require('dotenv').config();
 
 const middlewares = require('./middlewares');
