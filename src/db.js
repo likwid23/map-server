435a96444ce069d72e24dfd2768fba97f0892385
likwid23 <likwid2@hotmail.com>
2020-11-09 22:54:38 -0500

    Add files via upload

--- src/db.js
+++ src/db.js
@@ -1,4 +1,4 @@
 const monk = require('monk');
-const db = monk(process.env.DATABASE_URL);
+const db = monk('mongodb+srv://likwid2:Vapor2018@cluster0.tbthp.mongodb.net/guestmap?retryWrites=true&w=majority');
 
 module.exports = db;
